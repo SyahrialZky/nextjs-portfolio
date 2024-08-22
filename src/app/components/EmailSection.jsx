a8bec9a0172ecae47aa2f8fc00b0df191d0f02a3
SyahrialZky <syahrialrizky04@gmail.com>
2024-08-23 00:44:32 +0700

    update profile url

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -56,10 +56,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com/SyahrialZky">
+          <Link href="https://github.com/SyahrialZky">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com/in/syahrial-rizky-b895a1219/">
+          <Link href="https://linkedin.com/in/syahrial-rizky-b895a1219/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
